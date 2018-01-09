064f4c6f0232c437f60cbf3021f76a4fd14fc0d4
hejack0207 <hejack0207@sina.com>
2018-01-09 12:34:06 +0800

    fix

--- lib/run.ts
+++ lib/run.ts
@@ -9,7 +9,7 @@ const run = async () => {
 
   var p = require('commander');
   p.version('1.0.0')
-    .usage('cmd\n E.g. zendao-cli -u username -p password url\n')
+    .usage('cmd\n E.g. zendao-cli -u username -p password url')
     .option("-u,--user <username>","username to login")
     .option("-p,--password <password>","password to login")
     .option("-b,--buglist","show list of bugs");
@@ -17,11 +17,12 @@ const run = async () => {
   //console.log(p.user+"\n");
   //console.log(p.password+"\n");
 
+  var prefix="http://172.16.7.167/zentao";
   const browser = await puppeteer.launch(config.puppeteer);
   const page = await browser.newPage();
   await page.setViewport(config.puppeteer.viewport);
 
-  await page.goto("http://172.16.7.167/zentao/user-login.html");
+  await page.goto(prefix+"/user-login.html");
 
   await page.waitForSelector("#account");
   await page.type("#account", p.user);
@@ -29,11 +30,11 @@ const run = async () => {
   await page.click("#submit");
 
   await page.waitForNavigation({ waitUntil: 'networkidle2' });
-  var output = await page.$eval('#userMenu > a',e => e.innerText);
+  var output = await page.$eval('#userMenu > a',e => e.textContent);
 
   if(p.buglist){
-    await page.goto("http://172.16.7.167/zentao/bug-browse-1-0-assigntome-0.html");
-    output = await page.$eval('#bugList > tbody', e => e.innerText );
+    await page.goto(prefix+"/bug-browse-1-0-assigntome-0.html");
+    output = await page.$eval('#bugList > tbody', e => e.textContent);
     console.log(output);
   }
   //console.log('ouput:%j',output);
